1e876186d951d691019135d858940fa21e704335
Marc-André Dufresne <marc-andre.dufresne@novisto.com>
2021-10-28 17:26:16 -0400

    Improve number of API calls, add rate-limiting mitigation (#2)

--- examples/main.tf
+++ examples/main.tf
@@ -10,19 +10,41 @@ terraform {
 provider "bitwarden" {}
 
 locals {
-  db_creds = {
-    client : {
-      host : "host.example.com"
-      port : 5432
-      username : "user"
-      password : "df765287b64e51"
-    }
+  notes = {
+    "Note 00" = "NOTE 09"
+    "Note 01" = "NOTE 1"
+    "Note 02" = "NOTE 2"
+    "Note 03" = "NOTE 39"
+    "Note 04" = "NOTE 4"
+    "Note 05" = "NOTE 5"
+    "Note 06" = "NOTE 69"
+    "Note 07" = "NOTE 7"
+    "Note 08" = "NOTE 8"
+    "Note 09" = "NOTE 9"
+    "Note 10" = "NOTE 99"
+    "Note 11" = "NOTE 9"
+    "Note 12" = "NOTE 99"
+    "Note 13" = "NOTE 9"
+    "Note 14" = "NOTE 99"
+    "Note 15" = "NOTE 99"
+    "Note 16" = "NOTE 9"
+    "Note 17" = "NOTE 9"
+    "Note 18" = "NOTE 9"
+    "Note 19" = "NOTE 99"
+    "Note 20" = "NOTE 99"
+    "Note 21" = "NOTE 9"
+    "Note 22" = "NOTE 9"
+    "Note 23" = "NOTE 9"
+    "Note 24" = "NOTE 9"
+    "Note 25" = "NOTE 99"
   }
 }
 
-resource "bitwarden_secure_note" "platform_db_creds" {
+resource "bitwarden_secure_note" "platform_db_creds_1" {
+  for_each = local.notes
+
   organization_id = "df4736bb-2f70-47ac-98cb-ad7401042241"
   collection_ids  = ["d42f510e-6f45-404a-8a70-ad8d00f6cadf"]
-  name            = "Platform DB Credentials"
-  notes           = jsonencode(local.db_creds)
+  name            = each.key
+  notes           = each.value
 }
